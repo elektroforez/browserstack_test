f031952d2ce8e803ec78b098f50c44d86c905827
theend953 <theend953@gmail.com>
2025-05-02 18:22:49 +0200

    updated spec file

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,9 +1,9 @@
 const loginPage = require("../pages/login.page.js");
 const homePage = require("../pages/home.page.js");
 const SingInPage = require("../pages/singIn.page.js");
+const singInPage = require("../pages/singIn.page.js");
 const { expect } = require("@wdio/globals");
-import { faker } from "@faker-js/faker";
-import singInPage from "../pages/singIn.page.js";
+const { faker } = require("@faker-js/faker");
 
 var inputData = {
   email: faker.internet.email(),
@@ -17,14 +17,14 @@ describe("Login", () => {
     await homePage.goToLogin();
     await browser.pause(3000);
   });
-  it("should show error message on login with invalid data", async () => {
+  it("should show error message on login with invalid data | TC-MA-1", async () => {
     await loginPage.login(inputData.invalidEmail, inputData.invalidPassword);
     await browser.pause(3000);
     await expect(loginPage.emailError).toBeDisplayed();
     await expect(loginPage.passwordError).toBeDisplayed();
     await browser.pause(3000);
   });
-  it("should show error message on singin up with invalid email", async () => {
+  it("should show error message on singin up with invalid email | TC-MA-2", async () => {
     await loginPage.tapSingUpTab();
     await browser.pause(3000);
     await singInPage.singUp(
@@ -36,7 +36,7 @@ describe("Login", () => {
     await expect(singInPage.emailError).toBeDisplayed();
     await expect(singInPage.passwordError).toBeDisplayed();
   });
-  it("should show error message on singin up with different passwords", async () => {
+  it("should show error message on singin up with different passwords | TC-MA-3", async () => {
     await loginPage.tapSingUpTab();
     await browser.pause(3000);
     await singInPage.singUp(
@@ -47,7 +47,7 @@ describe("Login", () => {
     await browser.pause(3000);
     await expect(singInPage.repeatPasswordError).toBeDisplayed();
   });
-  it("should show success message on singin up with valid data", async () => {
+  it("should show success message on singin up with valid data | TC-MA-4", async () => {
     await singInPage.singUp(
       inputData.email,
       inputData.password,
@@ -56,7 +56,7 @@ describe("Login", () => {
     await browser.pause(9000);
     expect(singInPage.alert).toHaveText("Signed Up!");
   });
-  it("should show success message on login with valid data", async () => {
+  it("should show success message on login with valid data | TC-MA-5", async () => {
     await singInPage.closePopup();
     await singInPage.tapLoginButton();
     await browser.pause(3000);
